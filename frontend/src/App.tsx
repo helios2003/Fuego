939f98698efe49cc8ee1320dbc85181150c40743
Ankit Dash <ankitdash2019@gmail.com>
2024-04-19 22:51:34 +0530

    add 404

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import SignIn from "./components/auth/SignIn"
 import Preview from "./components/blogs/Preview"
 import Write from "./components/blogs/Write"
 import MainPage from "./components/dashboard/MainPage"
+import Error from "./components/utils/404"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/write" element={<Write />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/dashboard" element={<MainPage />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/components/utils/404.tsx
@@ -0,0 +1,9 @@
+export default function Error() {
+    return (
+      <div className="flex items-center justify-center flex-col gap-y-6">
+        <div className="text-4xl font-bold mt-4 text-blue-600">Page Not Found :(</div>
+        <img src="./error.png" alt="error" />
+      </div>
+    );
+  }
+  
